20ab002b51b242af32e547c6b3d976d72f2fdf49
ARBathriNarayanan <129174452+ARBathriNarayanan@users.noreply.github.com>
2025-12-24 10:53:51 +0000

    Deploying to gh-pages from @ ARBathriNarayanan/arbathrinarayanan.github.io@0710c31c38fd9c6d393e80163b7b9586bcd26a63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,42 +501,42 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/A%20R%20Bathri%20Narayanan%20CV.pdf", "_blank");
+        },
+      },{
+        id: 'social-discord',
+        title: 'Discord',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://discord.com/users/835063204937072660", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%72%62%61%74%68%72%69.%6E%61%72%61%79%61%6E%61%6E@%63%62%73.%61%63.%69%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/ARBathriNarayanan", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://instagram.com/bathri2003", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-quora',
+        title: 'Quora',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.quora.com/profile/A-R-Bathri-Narayanan", "_blank");
         },
       },{
       id: 'light-theme',
