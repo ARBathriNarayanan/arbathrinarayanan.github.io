6bd5f331e0d876e9396b119c89c71e8a378ab88f
ARBathriNarayanan <129174452+ARBathriNarayanan@users.noreply.github.com>
2026-03-03 15:08:06 +0000

    Deploying to gh-pages from @ ARBathriNarayanan/arbathrinarayanan.github.io@53dc098b0e0e57b077484e4f18a10e4a707d59a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "My Github UserID and repositories. All my programs used for various purposes can be accessed here",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
