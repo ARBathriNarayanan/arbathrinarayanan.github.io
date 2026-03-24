f7140021fe83def66634474ed1993f2f1f47efdb
ARBathriNarayanan <129174452+ARBathriNarayanan@users.noreply.github.com>
2026-03-24 07:19:36 +0000

    Deploying to gh-pages from @ ARBathriNarayanan/arbathrinarayanan.github.io@67e06617d109857fad14b8d1d8db5621b26369a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/bathriwrites/index.html";
           },
         },{id: "nav-projects",
           title: "Projects",
